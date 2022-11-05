e2d4f7ae3750d95ae800606cdeddf4947d0b0c43
Shamov-Aleksandr-RIS-21-1b <shamov.03@list.ru>
2022-11-05 21:07:13 +0500

    rename point.py & shape.py, add init & rewrite setter in Triangle

--- Lab2/triangle.py
+++ Lab2/triangle.py
@@ -2,10 +2,13 @@ import shape
 
 class Triangle(Shape):
 
+	def __init__(self, p1, p2, p3)
+		self.points.fset(self, p1, p2, p3)
+
 	@property
 	def points(self):
 		return super().points
 
 	@points.setter
-	def points(self, *args):
-		super().points.fset(self, *args)
+	def points(self, p1, p2, p3):
+		super().points.fset(self, p1, p2, p3)
