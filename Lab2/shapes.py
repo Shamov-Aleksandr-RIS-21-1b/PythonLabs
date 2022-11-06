316a7ff381493a4605b182581a70ca8fca2be1a5
Shamov-Aleksandr-RIS-21-1b <shamov.03@list.ru>
2022-11-07 01:05:50 +0500

    rename shape.py to shapes.py, transfer Triangle to shapes.py

--- Lab2/shapes.py
+++ Lab2/shapes.py
@@ -25,3 +25,21 @@ class Shape(ABC):
 	@abstractmethod
 	def get_square(self):
 		pass
+
+class Triangle(Shape):
+
+	def __init__(self, plist):
+		self.points = plist
+
+	@property
+	def points(self):
+		return super(Triangle, type(self)).points
+
+	@points.setter
+	def points(self, plist):
+		if len(plist) != 3:
+			raise AttributeError("too few or too many elements")
+		super(Triangle, type(self)).points.fset(self, plist)
+
+	def get_square():
+		pass
