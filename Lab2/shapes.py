8ec44068ab1e56fdcd4eff65e2c2b06795a0f06d
Shamov-Aleksandr-RIS-21-1b <shamov.03@list.ru>
2022-11-07 23:02:14 +0500

    add Rectangle class

--- Lab2/shapes.py
+++ Lab2/shapes.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod, abstractproperty
 from coordinates import *
+from statistics import mean
 
 class Shape(ABC):
 
@@ -49,3 +50,29 @@ class Triangle(Shape):
 		subside02 = Vector.scalarprod(vect01, vect02) / side02
 		height = (side01 ** 2 - subside02 ** 2) ** 0.5
 		return height * side02 / 2
+
+class Rectangle(Shape):
+
+	def __init__(self, plist):
+		self.points_set(plist)
+
+	def points_get(self):
+		return super(Rectangle, type(self)).points_get(self)
+
+	def points_set(self, plist):
+		if len(plist) != 4:
+			raise AttributeError("too few or too many elements")
+		super(Rectangle, type(self)).points_set(self, plist)
+
+	points = property(points_get, points_set)
+
+	def get_square(self):
+		vect01 = Vector()
+		vect01.set_from_points(self.points[0], self.points[1])
+		side01 = vect01.modulus()
+		vect03 = Vector()
+		vect03.set_from_points(self.points[0], self.points[3])
+		side03 = vect03.modulus()
+		subside03 = Vector.scalarprod(vect01, vect03) / side03
+		height = (side01 ** 2 - subside03 ** 2) ** 0.5
+		return height * side03
